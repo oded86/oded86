2b001803e10dcc798e6f26e6405d6c04164d3b8c
odedb@TLVL-79721175 <oded@amadeus.co.il>
2021-08-23 16:42:33 +0300

    deleted identity class

--- identity.py
+++
@@ -1,6 +0,0 @@
-class Identity:
-    def __init__(self):
-        self.consumer_key = 'OtQRP1rrp66KVWfvW69IWQ'
-        self.consumer_secret = 'wWMOwEGdb7xXGMUBVjstb1SgpOPzizzZJ469pMMJlo'
-        self.access_token = '31866248-Ha1usrlQu5F9mBfI9obN49tx99nxRy6mj7gBCw6I2'
-        self.access_token_secret = 'EQ2dxi4iZAXaoObteoBhCbv7G83k2VESsNxWrwwmW9cXy'
