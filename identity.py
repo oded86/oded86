acaff8235543ff965691af12fb88bef5d25eb9df
odedb@TLVL-79721175 <oded@amadeus.co.il>
2021-08-23 16:39:16 +0300

    Added venv file

--- identity.py
+++ identity.py
@@ -1,6 +1,6 @@
 class Identity:
     def __init__(self):
-        self.consumer_key = 'Add consumer_key'
-        self.consumer_secret = 'Add consumer_secret'
-        self.access_token = 'Add access_token'
-        self.access_token_secret = 'Add access_token_secret'
+        self.consumer_key = 'OtQRP1rrp66KVWfvW69IWQ'
+        self.consumer_secret = 'wWMOwEGdb7xXGMUBVjstb1SgpOPzizzZJ469pMMJlo'
+        self.access_token = '31866248-Ha1usrlQu5F9mBfI9obN49tx99nxRy6mj7gBCw6I2'
+        self.access_token_secret = 'EQ2dxi4iZAXaoObteoBhCbv7G83k2VESsNxWrwwmW9cXy'
