b5b37f6abf4654a64a179b954ec1f8e5725386cc
odedb@TLVL-79721175 <oded@amadeus.co.il>
2021-08-24 10:55:44 +0300

    added requirements.txt & setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonTwitter',
+    version='0.1',
+    packages=[''],
+    url='www.incontrol-sys.com',
+    license='MIT',
+    author='oded Bahar',
+    author_email='oded86+incontrol@gmail.com',
+    description='tweeter simple retweet bot'
+)
